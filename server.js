f51d7a79948a55126f8dc9a1de66c390f28641ff
Ron V <ronneyvargas@gmail.com>
2020-03-20 18:35:48 -0500

    adding port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post('/imageurl', (req, res) => {image.handleApiCall(req, res, db)});
 //   console.log(hash);
 // });
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
